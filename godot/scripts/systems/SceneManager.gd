9cf6a262ed1932f81d86df9778a40876dcdc8aff
unknown <bravoespinozaedwin@gmail.com>
2026-07-08 19:39:02 -0400

    Completar colisiones de la habitación inicial

--- godot/scripts/systems/SceneManager.gd
+++ godot/scripts/systems/SceneManager.gd
@@ -1,11 +1,16 @@
 extends Node
 
+var current_scene_container: Node = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func setup(container: Node) -> void:
+	current_scene_container = container
 
+func change_scene(scene_path: String) -> void:
+	if current_scene_container == null:
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	for child in current_scene_container.get_children():
+		child.queue_free()
+
+	var new_scene = load(scene_path).instantiate()
+	current_scene_container.add_child(new_scene)
